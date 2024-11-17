7518488ae190b92e7382b0322841905d74488d50
Levan Ilashvili <levan@ilashvili.me>
2024-11-17 05:04:44 +0400

    method for saving custodial loan address

--- src/ops/ops.service.ts
+++ src/ops/ops.service.ts
@@ -5,6 +5,9 @@ import { Repository } from 'typeorm';
 import { Cron, CronExpression, Interval } from '@nestjs/schedule';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { BalancesService } from 'src/balances/balances.service';
+import { CustodialService } from 'src/custodial/custodial.service';
+import { Loan } from './entities/loan.entity';
 
 @Injectable()
 export class OpsService {
@@ -13,11 +16,26 @@ export class OpsService {
     constructor(
         @InjectRepository(Lending)
         private readonly _lendingRepository: Repository<Lending>,
+        @InjectRepository(Loan)
+        private readonly _loanRepository: Repository<Loan>,
         private readonly httpService: HttpService,
+        private readonly balancesService: BalancesService,
+        private readonly custodialService: CustodialService,
     ) {}
 
     async saveLending(lending: Lending) {
         return await this._lendingRepository.save(lending);
+    }
+
+    async createLoan(userWallet: string) {
+        const loan = await this._loanRepository.save({ 
+            userWallet
+        });
+        const wallet = await this.custodialService.generateEthWallet(loan.walletId, false);
+        loan.depositAddress = wallet.publicKey;
+        loan.depositDerivationPath = wallet.path;
+        loan.status = 'pending';
+        await this._loanRepository.save(loan);
     }
 
     @Cron(CronExpression.EVERY_5_SECONDS)
